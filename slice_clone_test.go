3aed5e04d2ad7541e19fc6a3cc1d08c202fe7c6d
Laurent Demailly <ldemailly@gmail.com>
2023-05-08 22:42:29 -0700

    add a few more variants

--- slice_clone_test.go
+++ slice_clone_test.go
@@ -6,6 +6,16 @@ import (
 	"golang.org/x/exp/slices"
 )
 
+// Just for reference as obviously that's what copy() is for.
+// also to "warm up" the benchmarks.
+func Clone0(src []byte) []byte {
+	dst := make([]byte, 0, len(src))
+	for i := range src {
+		dst = append(dst, src[i])
+	}
+	return dst
+}
+
 func Clone1(src []byte) []byte {
 	dst := make([]byte, len(src))
 	copy(dst, src)
@@ -24,10 +34,22 @@ func Clone4(src []byte) []byte {
 	return dst
 }
 
-// benchmark compare Clone1, Clone2 and Clone3
-// go test -bench=. -benchmem
+func Clone5(src []byte) []byte {
+	return append([]byte(nil), src...)
+}
 
+// Clearly this is a small slice so only one of many cases...
 var byteSlice = []byte("hello world, how are you?")
+
+func BenchmarkClone0(b *testing.B) {
+	var res []byte
+	for i := 0; i < b.N; i++ {
+		res = Clone0(byteSlice)
+	}
+	if res[0] == 0 {
+		b.Log(res)
+	}
+}
 
 func BenchmarkClone1(b *testing.B) {
 	var res []byte
@@ -63,6 +85,16 @@ func BenchmarkClone4(b *testing.B) {
 	var res []byte
 	for i := 0; i < b.N; i++ {
 		res = Clone4(byteSlice)
+	}
+	if res[0] == 0 {
+		b.Log(res)
+	}
+}
+
+func BenchmarkClone5(b *testing.B) {
+	var res []byte
+	for i := 0; i < b.N; i++ {
+		res = Clone5(byteSlice)
 	}
 	if res[0] == 0 {
 		b.Log(res)
