9714a922b7cf57d0a4c6dea7bb091245447988fe
Laurent Demailly <ldemailly@gmail.com>
2023-11-11 13:08:20 -0800

    our own mini svg, also dev mode with local files vs embed mode for self contained binary

--- go.mod
+++ go.mod
@@ -6,7 +6,6 @@ require (
 	fortio.org/fortio v1.60.3
 	fortio.org/log v1.11.0
 	fortio.org/scli v1.12.0
-	github.com/ajstarks/svgo v0.0.0-20211024235047-1546f124cd8b
 	golang.org/x/exp v0.0.0-20231006140011-7918f672742d
 )
 
--- graph/graph.go
+++ graph/graph.go
@@ -1,7 +1,11 @@
 package main
 
 import (
+	"embed"
 	"flag"
+	"fmt"
+	"io"
+	"io/fs"
 	"net/http"
 	"os"
 	"strconv"
@@ -9,12 +13,34 @@ import (
 	"fortio.org/fortio/fhttp"
 	"fortio.org/log"
 	"fortio.org/scli"
-	svg "github.com/ajstarks/svgo"
 )
 
-// handler for serving index file
-func staticHandler(w http.ResponseWriter, r *http.Request) {
-	http.ServeFile(w, r, "index.html")
+// Embed the index.html file
+var (
+	//go:embed static/*
+	staticFS embed.FS
+)
+
+type SVG struct {
+	w io.Writer
+}
+
+func NewSVG(out io.Writer) *SVG {
+	svg := &SVG{w: out}
+	fmt.Fprint(out, "<svg>")
+	return svg
+}
+
+func (s *SVG) Circle(x, y, r int, style string) {
+	fmt.Fprintf(s.w, `<circle cx="%d" cy="%d" r="%d" style="%s"/>`, x, y, r, style)
+}
+
+func (s *SVG) Rect(x, y, w, h int, style string) {
+	fmt.Fprintf(s.w, `<rect x="%d" y="%d" width="%d" height="%d" style="%s"/>`, x, y, w, h, style)
+}
+
+func (s *SVG) End() {
+	fmt.Fprintln(s.w, "</svg>") // extra newline to separate multiple svg
 }
 
 func drawSVG(w http.ResponseWriter, r *http.Request) {
@@ -28,26 +54,42 @@ func drawSVG(w http.ResponseWriter, r *http.Request) {
 	// Set the content type to SVG
 	w.Header().Set("Content-Type", "image/svg+xml")
 	//w.Header().Set("Access-Control-Allow-Origin", "*") // For prod, replace * with specific allowed domain
-	canvas := svg.New(w)
-	canvas.Start(200, 200)
+	svg := NewSVG(w)
+	//canvas.Start(200, 200)
 	if i%2 == 0 {
-		canvas.Circle(50, 50, 20, "fill:green")
+		svg.Circle(50, 50, 20, "fill:green")
 	} else {
-		canvas.Rect(40, 40, 20, 20, "fill:red")
+		svg.Rect(40, 40, 20, 20, "fill:red")
 	}
-	canvas.Circle(150, 50, 20, "fill:green")
-	canvas.Circle(100, 150, 20, "fill:green")
-	canvas.End()
+	svg.Circle(150, 50, 20, "fill:green")
+	svg.Circle(100, 150, 20, "fill:green")
+	svg.End()
 }
 
 func main() {
 	port := flag.String("port", ":8080", "port to listen on")
+	dev := flag.Bool("dev", false, "dev mode (no embed)")
 	scli.ServerMain()
 	mux, addr := fhttp.HTTPServer("svg", *port)
 	if addr == nil {
 		os.Exit(1) // already logged
 	}
 	mux.HandleFunc("/svg", fhttp.LogAndCall("drawSVG", drawSVG))
-	mux.HandleFunc("/", fhttp.LogAndCall("static", staticHandler))
+	var staticHandler http.Handler
+	if *dev {
+		// So edits to static files are picked up without restart.
+		// go run . -dev # must be in this directory, parent of static/
+		staticHandler = http.FileServer(http.Dir("./static/"))
+	} else {
+		subFS, err := fs.Sub(staticFS, "static")
+		if err != nil {
+			log.Fatalf("Unable to get sub FS: %v", err)
+		}
+		staticHandler = http.FileServer(http.FS(subFS))
+	}
+	mux.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
+		log.LogRequest(r, "static")
+		staticHandler.ServeHTTP(w, r)
+	})
 	scli.UntilInterrupted()
 }
