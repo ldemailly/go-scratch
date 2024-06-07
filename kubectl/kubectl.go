fd19579e6f0094bfdef56d9a471f154611f2ad7b
Laurent Demailly <ldemailly@gmail.com>
2024-06-07 15:34:36 -0700

    improve output a bit and explain that empty == all namespaces

--- kubectl/kubectl.go
+++ kubectl/kubectl.go
@@ -15,8 +15,6 @@ import (
 )
 
 func main() {
-	fmt.Println("Get Kubernetes pods")
-
 	userHomeDir, err := os.UserHomeDir()
 	if err != nil {
 		fmt.Printf("error getting user home dir: %v\n", err)
@@ -34,7 +32,7 @@ func main() {
 		os.Exit(1)
 	}
 	namespace := rCfg.Contexts[rCfg.CurrentContext].Namespace
-	fmt.Printf("Using (default) namespace: %s\n", namespace)
+	fmt.Printf("Using (default) namespace: %q\n", namespace)
 	kubeConfig, err := cfg.ClientConfig()
 	if err != nil {
 		fmt.Printf("error getting Kubernetes config: %v\n", err)
@@ -45,7 +43,11 @@ func main() {
 		fmt.Printf("error getting Kubernetes clientset: %v\n", err)
 		os.Exit(1)
 	}
-
+	printedNS := namespace
+	if namespace == "" {
+		printedNS = "<all namespaces>"
+	}
+	fmt.Println("Get Kubernetes pods for namespace:", printedNS)
 	pods, err := clientset.CoreV1().Pods(namespace).List(context.Background(), v1.ListOptions{})
 	if err != nil {
 		fmt.Printf("error getting pods: %v\n", err)
