5bb0ab8b8dfbb805a3f5304d1e5bde572697dc04
Laurent Demailly <ldemailly@gmail.com>
2024-06-07 15:26:14 -0700

    get pods from context default config

---
+++ kubectl/kubectl.go
@@ -0,0 +1,57 @@
+// Modified from https://trstringer.com/connect-to-kubernetes-from-go/
+// to get the users's default namespace from their context
+
+package main
+
+import (
+	"context"
+	"fmt"
+	"os"
+	"path/filepath"
+
+	v1 "k8s.io/apimachinery/pkg/apis/meta/v1"
+	"k8s.io/client-go/kubernetes"
+	"k8s.io/client-go/tools/clientcmd"
+)
+
+func main() {
+	fmt.Println("Get Kubernetes pods")
+
+	userHomeDir, err := os.UserHomeDir()
+	if err != nil {
+		fmt.Printf("error getting user home dir: %v\n", err)
+		os.Exit(1)
+	}
+	kubeConfigPath := filepath.Join(userHomeDir, ".kube", "config")
+	fmt.Printf("Using kubeconfig: %s\n", kubeConfigPath)
+
+	cfg := clientcmd.NewNonInteractiveDeferredLoadingClientConfig(
+		&clientcmd.ClientConfigLoadingRules{ExplicitPath: kubeConfigPath}, nil)
+
+	rCfg, err := cfg.RawConfig()
+	if err != nil {
+		fmt.Printf("error getting raw config: %v\n", err)
+		os.Exit(1)
+	}
+	namespace := rCfg.Contexts[rCfg.CurrentContext].Namespace
+	fmt.Printf("Using (default) namespace: %s\n", namespace)
+	kubeConfig, err := cfg.ClientConfig()
+	if err != nil {
+		fmt.Printf("error getting Kubernetes config: %v\n", err)
+		os.Exit(1)
+	}
+	clientset, err := kubernetes.NewForConfig(kubeConfig)
+	if err != nil {
+		fmt.Printf("error getting Kubernetes clientset: %v\n", err)
+		os.Exit(1)
+	}
+
+	pods, err := clientset.CoreV1().Pods(namespace).List(context.Background(), v1.ListOptions{})
+	if err != nil {
+		fmt.Printf("error getting pods: %v\n", err)
+		os.Exit(1)
+	}
+	for _, pod := range pods.Items {
+		fmt.Printf("Pod name: %s %s\n", pod.Namespace, pod.Name)
+	}
+}
