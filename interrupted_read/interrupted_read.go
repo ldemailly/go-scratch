176a6687c4078cc9ed9b8177bf5c95fc93b98207
Laurent Demailly <ldemailly@gmail.com>
2024-07-03 10:32:01 -0700

    adding sample code for interrupt vs timeout vs blocking read

---
+++ interrupted_read/interrupted_read.go
@@ -0,0 +1,54 @@
+package main
+
+import (
+	"context"
+	"fmt"
+	"net"
+	"os"
+	"os/signal"
+	"syscall"
+	"time"
+
+	"fortio.org/log"
+)
+
+func main() {
+	ctx, cancel := context.WithTimeout(context.Background(), 10*time.Second)
+	defer cancel()
+
+	sigCh := make(chan os.Signal, 1)
+	signal.Notify(sigCh, syscall.SIGINT, syscall.SIGTERM)
+
+	conn, err := net.Dial("tcp", "localhost:8080")
+	if err != nil {
+		fmt.Println("Dial error:", err)
+		return
+	}
+	defer conn.Close()
+
+	buf := make([]byte, 1024)
+	for {
+		select {
+		case <-ctx.Done():
+			log.Printf("Read operation cancelled: %v", ctx.Err())
+			return
+		case sig := <-sigCh:
+			log.Printf("Received signal: %v", sig)
+			return
+		default:
+			conn.SetReadDeadline(time.Now().Add(3 * time.Second)) // Set read deadline
+			log.Printf("Entering read...")
+			n, err := conn.Read(buf)
+			log.Printf("Exiting read... %d %v", n, err)
+			if err != nil {
+				if ne, ok := err.(net.Error); ok && ne.Timeout() {
+					log.Printf("Read timeout, retrying...")
+					continue
+				}
+				log.Printf("Read error:", err)
+				return
+			}
+			log.Printf("Read %d bytes: %s", n, string(buf[:n]))
+		}
+	}
+}
