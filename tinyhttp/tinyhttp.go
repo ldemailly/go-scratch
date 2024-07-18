d06e5f60977250988c358c3107923ada6eb4d02a
Laurent Demailly <ldemailly@gmail.com>
2024-07-18 16:30:03 -0700

    working version finally, thanks Randy Reddig

--- tinyhttp/tinyhttp.go
+++ tinyhttp/tinyhttp.go
@@ -1,7 +1,18 @@
+/* Run this with:
+
+go install github.com/stealthrocket/wasi-go/cmd/wasirun@latest
+GOOS=wasip1 GOARCH=wasm go run -exec wasirun .
+
+23:26:19.565 r1 [INF] scli.go:125> Starting, command="tinyhttp", version="dev  go1.22.5 wasm wasip1", go-max-procs=1
+23:26:19.566 r1 [INF] tinyhttp.go:60> Listening on 0.0.0.0:8000
+23:26:19.566 r1 [INF] tinyhttp.go:61> Server entering listen
+23:26:22.659 r6 [INF]> http srv, method="GET", url="/", host="localhost:8000", proto="HTTP/1.1", remote_addr="127.0.0.1:59290", user-agent="curl/8.8.0", status=200, size=14, microsec=35
+
+*/
+
 package main
 
 import (
-	"context"
 	"errors"
 	"flag"
 	"net/http"
@@ -15,8 +26,8 @@ import (
 	"fortio.org/scli"
 
 	// these:
-	_ "github.com/stealthrocket/net/http"
-	"github.com/stealthrocket/net/wasip1"
+	// _ "github.com/stealthrocket/net/http" // not needed, that's for http client
+	"github.com/stealthrocket/net/wasip1" // needed to use that Listen instead of net.Listen
 )
 
 func helloHandler(w http.ResponseWriter, r *http.Request) {
@@ -46,26 +57,16 @@ func main() {
 	dflagEndpoint := endpoint.NewFlagsEndpoint(flag.CommandLine, "/flags/set")
 	mux.HandleFunc("/flags", log.LogAndCall("dflags-get", dflagEndpoint.ListFlags))
 	mux.HandleFunc("/flags/set", log.LogAndCall("dflags-set", dflagEndpoint.SetFlag))
-	l, err := wasip1.Listen("tcp", *port)
+	// Serve (accept) must be on main thread somehow - waspi restriction. BUG?
+	l, err := wasip1.Listen("tcp4", *port)
 	if err != nil {
 		log.Fatalf("Unable to listen: %v", err)
 	}
 	log.Infof("Listening on %v", l.Addr())
-	go func() {
-		log.Infof("Server entering listen")
-		err := server.Serve(l)
-		if err != nil && !errors.Is(err, http.ErrServerClosed) {
-			panic(err)
-		}
-		log.Infof("Server stopped listening")
-	}()
-	scli.UntilInterrupted()
-	ctx, cancel := context.WithTimeout(context.Background(), 5*time.Second)
-	defer cancel()
-	// Shut down the server
-	if err := server.Shutdown(ctx); err != nil {
-		log.Critf("Server Shutdown: %s", err)
-		return
+	log.Infof("Server entering listen")
+	err = server.Serve(l)
+	if err != nil && !errors.Is(err, http.ErrServerClosed) {
+		panic(err)
 	}
-	log.Infof("All done with graceful shutdown")
+	log.Infof("Server stopped listening") // not likely given we don't have Shutdown() anymore
 }
