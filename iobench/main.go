87a9dfe417f3c382a34fa9c5d9d1730df59b1734
Laurent Demailly <ldemailly@gmail.com>
2024-09-10 10:38:25 -0700

    allow mode to be passed without flag

--- iobench/main.go
+++ iobench/main.go
@@ -3,6 +3,7 @@ package main
 import (
 	"bufio"
 	"flag"
+	"fmt"
 	"io"
 	"math"
 	"os"
@@ -15,18 +16,24 @@ func main() {
 	var mode string
 	var bufferSize int
 	flag.StringVar(&filename, "filename", "genfile/random_lines.txt", "Output filename")
-	flag.StringVar(&mode, "mode", "blockio", "Mode to run: blockio, scanner, ...")
+	flag.StringVar(&mode, "mode", "blockio", "Mode to run: blockio, scanner, optio")
 	flag.IntVar(&bufferSize, "bufferSize", 64*1024, "Buffer size")
 	flag.Parse()
 	f, err := os.Open(filename)
 	if err != nil {
 		panic(err)
 	}
+	if len(flag.Args()) > 0 {
+		mode = flag.Args()[0]
+	}
+	fmt.Println("Running", mode, "on", filename, "with buffer size", bufferSize)
 	switch mode {
 	case "blockio":
 		processBlockio(f, bufferSize)
 	case "scanner":
 		processScanner(f, bufferSize)
+	case "optio":
+		processOptio(f, bufferSize)
 	default:
 		panic("Unknown/unimplemented mode " + mode)
 	}
@@ -80,4 +87,32 @@ func processScanner(f io.Reader, bufferSize int) {
 		}
 	}
 	println("BUFIO Scanner Lines:", lines, "minV:", minV, "Max:", maxV, "Total:", total)
+}
+
+func processOptio(f io.Reader, bufferSize int) {
+	minV := math.MaxInt
+	maxV := 0
+	total := 0
+	lines := 0
+	/*
+		s := optio.NewScanner(f)
+		s.Buffer(make([]byte, 0, bufferSize), bufferSize)
+
+		for s.Scan() {
+			line := s.Bytes()
+			length := len(line)
+			if length == 0 {
+				break
+			}
+			lines++
+			total += length
+			if length < minV {
+				minV = length
+			}
+			if length > maxV {
+				maxV = length
+			}
+		}
+	*/
+	println("OPTIO Lines:", lines, "minV:", minV, "Max:", maxV, "Total:", total)
 }
