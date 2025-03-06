31cf2a7b084b913f311d79e25cc43220e63f30f4
Laurent Demailly <ldemailly@gmail.com>
2025-03-06 11:50:55 -0800

    Use 1/8th-7/8th blocks too for increased resolution on the progress bar

--- progressbar/progressbar.go
+++ progressbar/progressbar.go
@@ -8,14 +8,26 @@ import (
 )
 
 const (
-	width = 60
+	width = 40
+	Space = " "
+	Full  = "█"
 )
+
+// 1/8th of a full block to 7/8ths of a full block (ie fractional part of a block to
+// get 8x resolution per character).
+var FractionalBlocks = [...]string{"", "▏", "▎", "▍", "▌", "▋", "▊", "▉"}
 
 // Show a progress bar from 0-1 (0-100%).
 func ProgressBar(progress float64) {
-	count := int(progress*width + 0.5)
-	bar := "[" + strings.Repeat("█", count) + strings.Repeat(" ", width-count) + "]"
-	fmt.Printf("\r%s %.2f%%", bar, progress*100)
+	count := int(8*progress*width + 0.5)
+	fullCount := count / 8
+	remainder := count % 8
+	spaceCount := width - fullCount - 1
+	if remainder == 0 {
+		spaceCount++
+	}
+	bar := "[" + strings.Repeat(Full, fullCount) + FractionalBlocks[remainder] + strings.Repeat(Space, spaceCount) + "]"
+	fmt.Printf("\r%s %.1f%%", bar, progress*100)
 }
 
 func MoveCursorUp(n int) {
@@ -28,13 +40,14 @@ func MoveCursorUp(n int) {
 
 func main() {
 	fmt.Print("Progress bar example\n\n")
-	for i := 0; i <= 1000; i++ {
-		ProgressBar(float64(i) / 1000)
+	n := width * 8 // just to demo every smooth step
+	for i := 0; i <= n; i++ {
+		ProgressBar(float64(i) / float64(n))
 		if i%63 == 0 {
 			MoveCursorUp(1)
-			fmt.Printf("%d of 1000\n", i)
+			fmt.Printf("Just an extra demo print for %d\n", i)
 		}
-		time.Sleep(10 * time.Millisecond)
+		time.Sleep(20 * time.Millisecond)
 	}
 	fmt.Println()
 }
