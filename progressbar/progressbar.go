940aee6ab906e9174e3a59446842e8a30f901b26
Laurent Demailly <ldemailly@gmail.com>
2025-03-06 12:06:02 -0800

    Add (optional, with -color) color and remove/replace the [] by color

--- progressbar/progressbar.go
+++ progressbar/progressbar.go
@@ -2,6 +2,7 @@
 package main
 
 import (
+	"flag"
 	"fmt"
 	"strings"
 	"time"
@@ -11,6 +12,9 @@ const (
 	width = 40
 	Space = " "
 	Full  = "█"
+	// Green FG, Grey BG
+	Color = "\033[32;47m"
+	Reset = "\033[0m"
 )
 
 // 1/8th of a full block to 7/8ths of a full block (ie fractional part of a block to
@@ -18,7 +22,7 @@ const (
 var FractionalBlocks = [...]string{"", "▏", "▎", "▍", "▌", "▋", "▊", "▉"}
 
 // Show a progress bar from 0-1 (0-100%).
-func ProgressBar(progress float64) {
+func ProgressBar(progress float64, useColors bool) {
 	count := int(8*progress*width + 0.5)
 	fullCount := count / 8
 	remainder := count % 8
@@ -26,7 +30,13 @@ func ProgressBar(progress float64) {
 	if remainder == 0 {
 		spaceCount++
 	}
-	bar := "[" + strings.Repeat(Full, fullCount) + FractionalBlocks[remainder] + strings.Repeat(Space, spaceCount) + "]"
+	color := Color
+	reset := Reset
+	if !useColors {
+		color = ""
+		reset = ""
+	}
+	bar := color + strings.Repeat(Full, fullCount) + FractionalBlocks[remainder] + strings.Repeat(Space, spaceCount) + reset
 	fmt.Printf("\r%s %.1f%%", bar, progress*100)
 }
 
@@ -39,10 +49,12 @@ func MoveCursorUp(n int) {
 }
 
 func main() {
+	colorFlag := flag.Bool("color", false, "Use color in the progress bar")
+	flag.Parse()
 	fmt.Print("Progress bar example\n\n")
 	n := width * 8 // just to demo every smooth step
 	for i := 0; i <= n; i++ {
-		ProgressBar(float64(i) / float64(n))
+		ProgressBar(float64(i)/float64(n), *colorFlag)
 		if i%63 == 0 {
 			MoveCursorUp(1)
 			fmt.Printf("Just an extra demo print for %d\n", i)
