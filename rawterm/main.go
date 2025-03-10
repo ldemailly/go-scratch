1cbc93267cacb11e9743a7c899af3ae989bc67a7
Laurent Demailly <ldemailly@gmail.com>
2025-03-09 18:24:10 -0700

    Clean up the code a bit

--- rawterm/main.go
+++ rawterm/main.go
@@ -9,13 +9,14 @@ import (
 )
 
 func main() {
-	oldState, err := term.MakeRaw(int(os.Stdin.Fd()))
+	fd := int(os.Stdin.Fd())
+	oldState, err := term.MakeRaw(fd)
 	if err != nil {
 		panic(err)
 	}
-	defer term.Restore(int(os.Stdin.Fd()), oldState)
+	defer term.Restore(fd, oldState)
 	fmt.Println("Try ^C, ^D, ^Z... Press 'q' to exit\r")
-	buf := make([]byte, 1)
+	buf := []byte{0} // 1 byte slice (could be  make([]byte, 1) instead)
 	for {
 		_, err := os.Stdin.Read(buf)
 		if err != nil {
@@ -23,7 +24,7 @@ func main() {
 			break
 		}
 		b := buf[0]
-		fmt.Printf("pressed: %d %q\r\n", b, string(b)) // need \r to in raw mode CR+LF.
+		fmt.Printf("pressed: %d %q\r\n", b, buf) // need \r too in raw mode: CR+LF.
 		if b == 'q' {
 			break
 		}
