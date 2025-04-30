1330a68724caa2929573ddcc917b2710f369a4be
Laurent Demailly <ldemailly@gmail.com>
2025-04-30 22:02:32 +0000

    add sample output

--- polkit/polkit.go
+++ polkit/polkit.go
@@ -1,3 +1,13 @@
+/*
+	Demonstrates (self) requesting for root using polkit pkexec: eg:
+
+$ go run github.com/ldemailly/go-scratch/polkit@2f6927ab6d45c8ec2d49572e2d86b1db3491a38e a b c d
+go: downloading github.com/ldemailly/go-scratch v0.1.3-0.20250430214433-2f6927ab6d45
+go: downloading github.com/ldemailly/go-scratch/polkit v0.0.0-20250430214433-2f6927ab6d45
+2025/04/30 21:58:07 Running [/usr/bin/pkexec /tmp/go-build491870754/b001/exe/polkit a b c d]
+Running as root 🎉
+Hello root world [/tmp/go-build491870754/b001/exe/polkit a b c d]
+*/
 package main
 
 import (
