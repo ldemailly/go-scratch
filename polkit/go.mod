903ce4febbf2850fd25d1edb1d430318d61ab9a8
Laurent Demailly <ldemailly@gmail.com>
2025-04-30 21:39:04 +0000

    Adding a polkit self exec demo

---
+++ polkit/go.mod
@@ -0,0 +1,3 @@
+module github.com/ldemailly/go-scratch/polkit
+
+go 1.24.2
---
+++ polkit/polkit.go
@@ -0,0 +1,37 @@
+package main
+
+import (
+	"fmt"
+	"log"
+	"os"
+	"os/exec"
+)
+
+const pkexec = "/usr/bin/pkexec"
+
+func doStuff(args []string) int {
+	// do stuff as root
+	fmt.Println("Hello root world", args)
+	return 0
+}
+
+func main() {
+	if os.Geteuid() == 0 {
+		fmt.Println("Running as root 🎉")
+		os.Exit(doStuff(os.Args))
+	}
+	// get our name and args
+	args := []string{pkexec}
+	args = append(args, os.Args...)
+	sub := exec.Cmd{
+		Path: pkexec,
+		Args: args,
+	}
+	log.Printf("Running %v", sub)
+	sub.Stdout = os.Stdout
+	sub.Stderr = os.Stderr
+	err := sub.Run()
+	if err != nil {
+		log.Fatal(err)
+	}
+}
