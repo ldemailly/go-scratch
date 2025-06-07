756b37f72cfa14d7df6d75895f7ffd2356fe5d11
Laurent Demailly <ldemailly@gmail.com>
2025-06-06 18:59:34 -0700

    Adding repro for https://github.com/tractordev/wanix/issues/187

---
+++ wanix/go.mod
@@ -0,0 +1,1 @@
+module github.com/ldemailly/go-scratch/wanix
---
+++ wanix/wanix.go
@@ -0,0 +1,11 @@
+// "make" and drop repro.wasm in https://wanix.run/
+// ./repro.wasm // hangs
+package main
+
+import (
+	"fmt"
+)
+
+func main() {
+	fmt.Println("hello world of wanix.")
+}
