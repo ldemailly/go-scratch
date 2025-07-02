50d6e7e3cc29268a9c4d045fb0019fec420eea11
Laurent Demailly <ldemailly@gmail.com>
2025-07-02 14:06:59 -0700

    Adding confirmation of lock vs no lock to stderr and header/comment

--- lognolock/loglocknolock.go
+++ lognolock/loglocknolock.go
@@ -1,13 +1,28 @@
+/*
+  Evaluate the effect on performance of lock protecting Write calls (which log and slog do).
+  Test correctness:
+    go run . | sort | uniq -c
+  Test performance:
+    go build .
+    hyperfine --warmup 3 --runs 10  "./lognolock > /dev/null"
+  vs
+    hyperfine --warmup 3 --runs 10  "./lognolock  -lock > /dev/null"
+
+  On macos m3 pro: with lock is ~150ms and faster than without lock ~200ms. (!!)
+*/
+
 package main
 
 import (
 	"flag"
+	"fmt"
 	"os"
 	"strconv"
 	"sync"
 )
 
 const N = 20
+const M = 10000
 
 func main() {
 	doLockFlag := flag.Bool("lock", false, "use a mutex to lock Write calls")
@@ -15,12 +30,13 @@ func main() {
 	doLock := *doLockFlag
 	var l sync.Mutex
 	var wg sync.WaitGroup
+	fmt.Fprintf(os.Stderr, "Running %d goroutines with %d Write in each; Using lock: %t\n", N, M, doLock)
 	for i := 0; i < N; i++ {
 		wg.Add(1)
 		go func(id int) {
 			// Write a unique line using a single Write call
 			msg := "Goroutine " + strconv.Itoa(id) + "\n"
-			for range 10000 {
+			for range M {
 				if doLock {
 					l.Lock()
 				}
