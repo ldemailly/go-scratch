928ec07cbb0d384343430bd330ec5e1a83db29b2
Laurent Demailly <ldemailly@gmail.com>
2025-09-03 17:16:54 -0700

    Cleanup a bit

--- nilisfine/nilisfine.go
+++ nilisfine/nilisfine.go
@@ -1,9 +1,9 @@
+// Demonstrate nil dereference without panic/segv, works on Linux after
+// echo 0 > /proc/sys/vm/mmap_min_addr
 // ported from https://github.com/ldemailly/experimental/blob/master/misc-c/happynull.c
 package main
 
 /*
-// Works on Linux after
-// echo 0 > /proc/sys/vm/mmap_min_addr
 #include <sys/mman.h>
 #include <stdio.h>
 #include <stdlib.h>
@@ -15,15 +15,16 @@ static void pagingDrZero(void) {
   void *p = mmap(start, 4096, PROT_READ | PROT_WRITE, MAP_FIXED | MAP_ANON | MAP_PRIVATE, -1, 0);
   if (errno != 0) {
       perror("Failed page 0 alloc you need to run:\n\necho 0 > /proc/sys/vm/mmap_min_addr\n\nso normal segv/panic will happen");
-  } else {
-    printf("Allocated page at %p\n", p);
   }
 }
 */
 import "C"
+import "fmt"
 
 func main() {
 	var p *int
+	fmt.Printf("Before nil deref: p=%p\n", p)
 	// Dereferencing a nil pointer, won't crash if the above worked.
 	*p = 42
+	fmt.Printf("after nil deref: p=%p, *p=%d\n", p, *p)
 }
