6b2011560e247c4ac2fa29d852dff37b4414bbd8
Laurent Demailly <ldemailly@gmail.com>
2025-09-24 13:21:10 -0700

    Individually address each number and demo with color

--- sieve/sieve.go
+++ sieve/sieve.go
@@ -3,23 +3,23 @@ package main
 
 import (
 	"flag"
-	"fmt"
 
 	"fortio.org/terminal/ansipixels"
+	"fortio.org/terminal/ansipixels/tcolor"
 )
 
 // returns the N and width of each cell and how many cell per line - eg 700 and 4 (3+1) and p, or 2300 and 5 (4+1) and q.
 func calcN(height, width int) (int, int, int) {
 	// Find number of digits (start with 3 and try 4)
-	spaceUsedPer := 3 + 1 // + 1 space at least
-	perLine := width / spaceUsedPer
+	spaceUsedPer := 3 + 1                 // + 1 space in between
+	perLine := (width + 1) / spaceUsedPer // + 1 because last space is not needed
 	total := perLine * height
 	if total < 1000 {
 		return total, spaceUsedPer, perLine
 	}
 	// try 4
 	spaceUsedPer++
-	nPerLine := width / spaceUsedPer
+	nPerLine := (width + 1) / spaceUsedPer
 	ntotal := nPerLine * height
 	// could be back to 3 digits on border cases
 	if ntotal < 1000 {
@@ -32,6 +32,22 @@ func calcN(height, width int) (int, int, int) {
 	return ntotal, spaceUsedPer, nPerLine
 }
 
+type State struct {
+	ap      *ansipixels.AnsiPixels
+	n       int
+	padding int
+	perLine int
+}
+
+func (s *State) NumberAt(n int) (int, int) {
+	return ((n - 1) % s.perLine) * s.padding, (n - 1) / s.perLine
+}
+
+func (s *State) ShowNumberAt(n int, c tcolor.Color) {
+	x, y := s.NumberAt(n)
+	s.ap.WriteAt(x, y, "%s%d", s.ap.ColorOutput.Foreground(c), n)
+}
+
 func main() {
 	fps := flag.Float64("fps", 60.0, "Frames per second")
 	flag.Parse()
@@ -41,21 +57,19 @@ func main() {
 		panic(err)
 	}
 	defer ap.Restore()
+	s := &State{ap: ap}
 	ap.OnResize = func() error {
 		ap.ClearScreen()
-		// Calculate sieve dimensions:
-		n, space, perLine := calcN(ap.H, ap.W)
-		for i := 1; i <= n; i++ {
-			if i != 1 && (i-1)%perLine == 0 {
-				fmt.Fprintln(ap.Out, "\r")
-			}
-			fmt.Fprintf(ap.Out, "%*d", space, i)
+		// Calculate and save sieve dimensions:
+		s.n, s.padding, s.perLine = calcN(ap.H, ap.W)
+		for i := 1; i <= s.n; i++ {
+			s.ShowNumberAt(i, tcolor.HSLf(float64(i)/float64(s.n+1), 0.5, 0.5))
 		}
 		return nil
 	}
 	_ = ap.OnResize()
 	for {
-		ap.WriteBoxed(ap.H/2, "Resize me, Q or ^C to quit")
+		ap.WriteBoxed(ap.H/2, "%sResize me, Q or ^C to quit", tcolor.Reset)
 		err := ap.ReadOrResizeOrSignal()
 		if err != nil {
 			panic(err)
