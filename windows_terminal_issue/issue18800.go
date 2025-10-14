8ee877d49bbec7394d2246f31c59298b888363dc
Laurent Demailly <ldemailly@gmail.com>
2025-10-14 09:54:33 -0700

    Print fps over chunks

--- windows_terminal_issue/issue18800.go
+++ windows_terminal_issue/issue18800.go
@@ -1,5 +1,10 @@
-// https://github.com/microsoft/terminal/issues/18800
-
+// This was initially created as minimal repro of a hang observed with windows terminal
+// with https://github.com/fortio/fps#fps : https://github.com/microsoft/terminal/issues/18800
+// It turns out it also reproduces a slow down / issue with Ghostty
+// https://github.com/ghostty-org/ghostty/discussions/9187
+// Note that this doesn't (yet) reproduce the slow down over time, only that 1.1.3 is faster
+// than 1.2.x/tip. It does repro an even bigger slow down if ran _after_ fps (with something
+// left on screen, and a bit better after a clear screen).
 package main
 
 import (
@@ -7,6 +12,7 @@ import (
 	"fmt"
 	"os"
 	"strings"
+	"time"
 
 	"golang.org/x/term"
 )
@@ -17,8 +23,10 @@ func RawPrintln(stuff ...any) {
 }
 
 func main() {
-	numIter := 100_000
+	numIter := 1_000_000
+	timeEvery := 100_000
 	flag.IntVar(&numIter, "n", numIter, "number of `iterations`")
+	flag.IntVar(&timeEvery, "t", timeEvery, "dump timing every `t` iterations")
 	flag.Parse()
 	fd := int(os.Stdin.Fd())
 	oldState, err := term.MakeRaw(fd)
@@ -35,7 +43,8 @@ func main() {
 	buf := make([]byte, 16) // fits ansi arrow escape, unicode, etc
 	requestCursorPos := []byte("\033[6n")
 	expected := len(requestCursorPos)
-	for iter := range numIter {
+	now := time.Now()
+	for iter := 1; iter <= numIter; iter++ {
 		nw, err := os.Stdout.Write(requestCursorPos)
 		if err != nil {
 			RawPrintln("Error writing to terminal:", err)
@@ -56,6 +65,13 @@ func main() {
 			break
 		}
 		fmt.Printf("\r[%05d] Read %d bytes: %q      ", iter, n, bufStr)
+		if iter%timeEvery == 0 {
+			delta := time.Since(now)
+			ops := float64(timeEvery) / delta.Seconds()
+			delta = delta.Round(time.Millisecond)
+			fmt.Printf("\t-- at %d took %v -- %.1f fps\r\n", iter, delta, ops)
+			now = time.Now()
+		}
 	}
 	RawPrintln("\r\nDone")
 }
