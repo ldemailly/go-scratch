cb785ff0dd8821e243f0474f2fabf6df6ca0bdef
Laurent Demailly <ldemailly@gmail.com>
2025-11-15 09:33:25 -0800

    ansipixel out kinda working

--- sshdtui/go.mod
+++ sshdtui/go.mod
@@ -5,6 +5,7 @@ go 1.24.0
 require (
 	fortio.org/log v1.18.3
 	fortio.org/scli v1.19.0
+	fortio.org/terminal v0.61.0
 	github.com/gliderlabs/ssh v0.3.9-0.20250127223818-909fa952d4e4
 	golang.org/x/crypto v0.43.0
 )
@@ -13,12 +14,17 @@ require (
 	fortio.org/cli v1.12.3 // indirect
 	fortio.org/dflag v1.9.3 // indirect
 	fortio.org/duration v1.0.4 // indirect
+	fortio.org/safecast v1.2.0 // indirect
 	fortio.org/sets v1.3.0 // indirect
 	fortio.org/struct2env v0.4.2 // indirect
 	fortio.org/version v1.0.4 // indirect
 	github.com/anmitsu/go-shlex v0.0.0-20200514113438-38f4b401e2be // indirect
 	github.com/fsnotify/fsnotify v1.8.0 // indirect
+	github.com/jbuchbinder/gopnm v0.0.0-20220507095634-e31f54490ce0 // indirect
 	github.com/kortschak/goroutine v1.1.3 // indirect
-	golang.org/x/crypto/x509roots/fallback v0.0.0-20250203165127-fa5273e46196 // indirect
+	github.com/rivo/uniseg v0.4.7 // indirect
+	golang.org/x/crypto/x509roots/fallback v0.0.0-20250406160420-959f8f3db0fb // indirect
+	golang.org/x/image v0.30.0 // indirect
 	golang.org/x/sys v0.38.0 // indirect
+	golang.org/x/term v0.36.0 // indirect
 )
--- sshdtui/sshd.go
+++ sshdtui/sshd.go
@@ -1,15 +1,16 @@
 package main
 
 import (
+	"bufio"
 	"crypto/rand"
 	"crypto/rsa"
 	"encoding/pem"
-	"fmt"
 	"os"
 	"time"
 
 	"fortio.org/log"
 	"fortio.org/scli"
+	"fortio.org/terminal/ansipixels"
 	"github.com/gliderlabs/ssh"
 	gossh "golang.org/x/crypto/ssh"
 )
@@ -20,17 +21,33 @@ func Handler(s ssh.Session) {
 	p, c, ok := s.Pty()
 	log.S(log.Info, "Pty:", log.Any("pty", p), log.Any("ok", ok))
 	width, height := p.Window.Width, p.Window.Height
-	fmt.Fprintf(s, "Hello, SSH!\nInitial terminal width: %d, height: %d\nResize me!\n", width, height)
+	ap := ansipixels.AnsiPixels{
+		Out: bufio.NewWriter(s),
+		FPS: 60,
+		H:   height,
+		W:   width,
+	}
+	ap.ClearScreen()
+	ap.WriteBoxed(ap.H/2-1, "Hello, SSH!\nInitial terminal width: %d, height: %d\nResize me!", width, height)
+	ap.EndSyncMode()
+
 	for {
 		select {
+		// case <-s.Read():
 		case w := <-c:
 			if w.Width == width && w.Height == height {
 				continue
 			}
 			width, height = w.Width, w.Height
-			fmt.Fprintf(s, "Window size changed: width=%d, height=%d\n", width, height)
+			ap.H = height
+			ap.W = width
+			ap.ClearScreen()
+			ap.WriteBoxed(ap.H/2-3, "Window size changed:\n%d x %d ", width, height)
+			ap.EndSyncMode()
 		case <-time.After(10 * time.Second):
-			fmt.Fprintf(s, "No window size change for 10 seconds, closing session.\n")
+			ap.WriteAt(0, ap.H-2, "No window size change for 10 seconds, closing session.")
+			ap.MoveCursor(0, ap.H-1)
+			ap.EndSyncMode()
 			return
 		}
 	}
