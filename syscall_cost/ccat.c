d67c10adbba6a33262657d11db27eb7cd02056bb
Laurent Demailly <ldemailly@gmail.com>
2026-01-06 11:47:28 -0800

    add pure syscall go version to compare fairly

--- syscall_cost/ccat.c
+++ syscall_cost/ccat.c
@@ -5,16 +5,18 @@ enum { bufSize = 1024 };
 
 int main(void) {
     char buf[bufSize];
-    ssize_t bytesRead, bytesWritten;
+    ssize_t bytesRead, bytesWritten, total = 0;
     while ((bytesRead = read(STDIN_FILENO, buf, bufSize)) > 0) {
         bytesWritten = write(STDOUT_FILENO, buf, bytesRead);
         if (bytesWritten < 0) {
             perror("write");
             return bytesWritten;
         }
+        total += bytesWritten;
     }
     if (bytesRead < 0) {
         perror("read");
     }
+    fprintf(stderr, "%zd\n", total);
     return bytesRead;
 }
--- syscall_cost/gocat.go
+++ syscall_cost/gocat.go
@@ -2,19 +2,23 @@ package main
 
 import (
 	"errors"
+	"flag"
+	"fmt"
 	"io"
 	"log"
 	"os"
+	"syscall"
 )
 
 const bufSize = 1024
 
-func main() {
-	buf := make([]byte, bufSize)
+func fileCat() int64 {
+	var buf [bufSize]byte
 	var err error
 	var r, w int
+	var total int64
 	for {
-		r, err = os.Stdin.Read(buf)
+		r, err = os.Stdin.Read(buf[:])
 		if r > 0 {
 			w, err = os.Stdout.Write(buf[:r])
 			if err != nil {
@@ -23,6 +27,7 @@ func main() {
 			if w != r {
 				log.Fatalf("write mismatch %d %d", w, r)
 			}
+			total += int64(w)
 		}
 		if err != nil {
 			break
@@ -31,4 +36,45 @@ func main() {
 	if !errors.Is(err, io.EOF) {
 		log.Fatalf("read error: %v", err)
 	}
+	return total
+}
+
+func syscallCat() int64 {
+	var buf [bufSize]byte
+	var err error
+	var r, w int
+	var total int64
+	for {
+		r, err = syscall.Read(0, buf[:])
+		if err != nil || r < 0 {
+			break
+		}
+		if r == 0 {
+			return total
+		}
+		w, err = syscall.Write(1, buf[:r])
+		if err != nil {
+			log.Fatalf("write error %d %v", w, err)
+		}
+		if w != r {
+			log.Fatalf("write mismatch %d %d", w, r)
+		}
+		total += int64(w)
+	}
+	if !errors.Is(err, io.EOF) {
+		log.Fatalf("read error: %v", err)
+	}
+	return total
+}
+
+func main() {
+	syscall := flag.Bool("syscall", false, "use syscall directly for I/O (file otherwise)")
+	flag.Parse()
+	var total int64
+	if *syscall {
+		total = syscallCat()
+	} else {
+		total = fileCat()
+	}
+	fmt.Fprintf(os.Stderr, "%d\n", total)
 }
