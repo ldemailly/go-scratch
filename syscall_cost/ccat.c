3108060575010ccf324bf228d167501dc4a70bb7
Laurent Demailly <ldemailly@gmail.com>
2026-01-06 12:52:10 -0800

    Add check on number of system calls (they are the same)

--- syscall_cost/ccat.c
+++ syscall_cost/ccat.c
@@ -5,18 +5,28 @@ enum { bufSize = 128 }; // small on purpose to get lots of syscalls
 
 int main(void) {
     char buf[bufSize];
-    ssize_t bytesRead, bytesWritten, total = 0;
-    while ((bytesRead = read(STDIN_FILENO, buf, bufSize)) > 0) {
+    ssize_t bytesRead, bytesWritten, total = 0, numCall =0;
+    for (;;) {
+        numCall++;
+        bytesRead = read(STDIN_FILENO, buf, bufSize);
+        if (bytesRead <= 0) {
+            break;
+        }
+        numCall++;
         bytesWritten = write(STDOUT_FILENO, buf, bytesRead);
         if (bytesWritten < 0) {
             perror("write");
             return bytesWritten;
         }
         total += bytesWritten;
+        if (bytesWritten != bytesRead) {
+            fprintf(stderr, "write mismatch %zd %zd\n", bytesWritten, bytesRead);
+            return -1;
+        }
     }
     if (bytesRead < 0) {
         perror("read");
     }
-    fprintf(stderr, "%zd\n", total);
+    fprintf(stderr, "%zd (%zd)\n", total, numCall);
     return bytesRead;
 }
--- syscall_cost/gocat.go
+++ syscall_cost/gocat.go
@@ -12,14 +12,17 @@ import (
 
 const bufSize = 128 // small on purpose to get lots of syscalls
 
-func fileCat() int64 {
+func fileCat() (int64, int) {
 	var buf [bufSize]byte
 	var err error
 	var r, w int
 	var total int64
+	var numCall int
 	for {
+		numCall++
 		r, err = os.Stdin.Read(buf[:])
 		if r > 0 {
+			numCall++
 			w, err = os.Stdout.Write(buf[:r])
 			if err != nil {
 				log.Fatalf("write error %d %v", w, err)
@@ -36,22 +39,26 @@ func fileCat() int64 {
 	if !errors.Is(err, io.EOF) {
 		log.Fatalf("read error: %v", err)
 	}
-	return total
+	return total, numCall
 }
 
-func syscallCat() int64 {
+func syscallCat() (int64, int) {
 	var buf [bufSize]byte
 	var err error
-	var r, w int
+	var r int
+	var w int
 	var total int64
+	var numCall int
 	for {
+		numCall++
 		r, err = syscall.Read(0, buf[:])
 		if err != nil || r < 0 {
 			break
 		}
 		if r == 0 {
-			return total
+			return total, numCall
 		}
+		numCall++
 		w, err = syscall.Write(1, buf[:r])
 		if err != nil {
 			log.Fatalf("write error %d %v", w, err)
@@ -64,17 +71,18 @@ func syscallCat() int64 {
 	if !errors.Is(err, io.EOF) {
 		log.Fatalf("read error: %v", err)
 	}
-	return total
+	return total, numCall
 }
 
 func main() {
 	syscall := flag.Bool("syscall", false, "use syscall directly for I/O (file otherwise)")
 	flag.Parse()
 	var total int64
+	var numCall int
 	if *syscall {
-		total = syscallCat()
+		total, numCall = syscallCat()
 	} else {
-		total = fileCat()
+		total, numCall = fileCat()
 	}
-	fmt.Fprintf(os.Stderr, "%d\n", total)
+	fmt.Fprintf(os.Stderr, "%d (%d)\n", total, numCall)
 }
